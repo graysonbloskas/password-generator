b597eb85e313ac958855d22f13adfa72b8506b7f
Grayson Bloskas <graysonbloskas@gmail.com>
2021-03-26 20:05:05 -0400

    Almost final code. The password generator is working. I just need to add a if NaN/parse int function. Will do later tonight.

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-// Assignment Code
+var lowLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upLetter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "~", "?", ">", "<", ":", ";", "[", "{", "]", "}"];
 
+var userChar = [];
+
+// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -11,11 +17,48 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+// Adds an event listener "clicl" to the site.
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
 
-  var passwordLength = prompt("Enter the length of your password");
-  console.log(passwordLength);
+  var passwordLength = prompt("Enter the desired length of your password");
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Password must contain at least 8 characters and a max of 128 characters");
+    return;
+  }
+
+  else {
+  var lower = confirm("Do you want to add lower case letters?");
+  var upper = confirm("Do you want to add UPPER CASE LETTERS?");
+  var num = confirm("Do you want to add numbers?");
+  var spec = confirm("Do you want to add special characters?");
+
+
+  if (lower) {
+    userChar = userChar.concat(lowLetter);
+    
+  }
+  
+  if (upper) {
+    userChar = userChar.concat(upLetter);
+  
+  }
+  
+  if (num) {
+    userChar = userChar.concat(number);
+  
+  }
+  
+  if (spec) {
+    userChar = userChar.concat(special);
+  }
 }
+  var newString = "";
+
+    for (i = 0; i < passwordLength ; ++i) {
+      var index = Math.floor(Math.random() * userChar.length);
+      newString += userChar[index];
+    }
+return newString;
+  }
